daaad350916e4448c44bfb5653fe7547d8c6e8b9
Orstedra <daeusosenna@gmail.com>
2026-09-14 23:00:28 +0200

    Auto-download packwiz-installer.jar if missing in update.bat

--- update.bat
+++ update.bat
@@ -8,8 +8,13 @@ if not defined INST_DIR set "INST_DIR=%~dp0"
 cd /d "%INST_MC_DIR%"
 
 if not exist "packwiz-installer-bootstrap.jar" (
-    echo Downloading packwiz-installer-bootstrap.jar...
-    powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar' -OutFile 'packwiz-installer-bootstrap.jar'"
+    echo [PionPack] Downloading packwiz-installer-bootstrap.jar...
+    curl.exe -fL -o "packwiz-installer-bootstrap.jar" "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar"
+)
+
+if not exist "packwiz-installer.jar" (
+    echo [PionPack] Downloading packwiz-installer.jar...
+    curl.exe -fL -o "packwiz-installer.jar" "https://github.com/packwiz/packwiz-installer/releases/download/v0.5.14/packwiz-installer.jar"
 )
 
 "%INST_JAVA%" -jar packwiz-installer-bootstrap.jar --bootstrap-no-update https://raw.githubusercontent.com/Orstedra/PionPack/main/pack.toml
